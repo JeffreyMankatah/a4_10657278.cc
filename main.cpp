1c424cadfa397dd10df4d5cbe14abb86678ed382
JeffreyMankatah <42754832+JeffreyMankatah@users.noreply.github.com>
2018-09-13 06:21:51 +0000

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int marks;
+    cout << "Please enter your marks" <<endl;
+    cin>> marks;
+
+    switch (marks)
+    {
+    case '80 <= 100':
+        cout << "A+";
+    break;
+    }
+    return 0;
+}
